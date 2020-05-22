25494e888b53ae50a2ce859143d0315a191b5489
gunjan4098 <65719034+gunjan4098@users.noreply.github.com>
2020-05-22 13:19:51 +0530

    Create Program5.py

---
+++ Day1/Program5.py
@@ -0,0 +1,15 @@
+p1=input("Runs scored by Player 1 = ")
+p2=input("Runs scored by Player 2 = ")
+p3=input("Runs scored by Player 3 = ")
+s1=(float(p1)/60)*100
+s2=(float(p2)/60)*100
+s3=(float(p3)/60)*100
+print("Strike rate of Player 1 = ",s1)
+print("Strike rate of Player 2 =",s2)
+print("Strike rate of Player 3 = ",s3)
+a=s1*2
+b=s2*2
+c=s3*2
+print("Runs scored if Player 1 had played 60 more balls = ",a)
+print("Runs scored if Player 2 had played 60 more balls = ",b)
+print("Runs scored if Player 3 had played 60 more balls = ",c)
